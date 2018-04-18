5c5cfb5387af863a053c71275b2dbe07985ed437
FauricioValencia <julian.f.valencia@hotmail.com>
2018-04-18 15:15:14 -0500

    Agrego la estructura de carpetas para el proyecto

---
+++ ArduinoMPU/RookieGameArduino/RookieGameArduino.ino
@@ -0,0 +1,111 @@
+#include <Wire.h>
+
+//Direccion I2C de la IMU
+const int  MPU =0x68;
+
+//Ratios de conversion 
+#define A_R 16384.0
+#define G_R 131.0
+
+//Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
+
+//MPU-6050 da los valores en enteros de 16 bits
+//Valores sin refinar
+int16_t AcX, AcY, AcZ,GyX, GyY, GyZ;
+
+//Angulos
+float Acc[3];
+float Gy[3];
+
+float Angle1;
+float Angle2;
+float Angle3;
+
+//Variable joystick
+int xPin = A0;
+int yPin = A1;
+int Pulsador = 2;
+
+int xPosicion = 0;
+int yPosicion = 0;
+int EstadoPulsador =0;
+
+void setup() {
+  // put your setup code here, to run once:
+  //Inicio setup MPU
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+  //Fin setup MPU
+ 
+//Joystick
+ //Serial.begin(9600);
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+
+  pinMode(Pulsador, INPUT);
+  digitalWrite(Pulsador, HIGH);
+  //fin setup Joystick
+}
+
+void loop() {
+
+  //Inicio Lopp MPU
+  // put your main code here, to run repeatedly:
+  //Leer los valores del Acelerometro de la IMU
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);//Pedir el registro 0x3B - corresponde al AcX
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,6,true);//A parti del 0x3B, se piden 6 registros
+  AcX=Wire.read()<<8|Wire.read();//Cada valor ocupa 2 registros
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+
+  //A partir de los valores del acelerometro se calculan los angulosY,X
+  //respectivamente con la formula de la tangente
+  Acc[0] =atan((AcX/A_R)    /sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2))) * RAD_TO_DEG;
+  Acc[1] =atan((AcY/A_R)    /sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2))) * RAD_TO_DEG;
+  Acc[2] =atan((AcZ/A_R)    /sqrt(pow((AcX/A_R),2) + pow((AcY/A_R),2))) * RAD_TO_DEG;
+
+  //Leer los valores del Giroscopio
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,4,true);//A diferencia del acelerometro solo se piden 4 registros
+  GyX =Wire.read()<<8|Wire.read();
+  GyY =Wire.read()<<8|Wire.read();
+  GyZ =Wire.read()<<8|Wire.read();
+
+  //Calculo de angulos
+  Gy[0] = GyX/G_R;
+  Gy[1] = GyY/G_R;
+  Gy[2] = GyZ/G_R;
+
+  //Aplicar el filtro Complementario
+  Angle1 = 0.98*(Angle1+Gy[0]*0.010) + 0.02*Acc[0];
+  Angle2 = 0.98*(Angle2+Gy[1]*0.010) + 0.02*Acc[1];
+  Angle3 = 0.98*(Angle3+Gy[2]*0.010) + 0.02*Acc[2];
+
+  //Mostrar los valores por consola
+  //Serial.print("Angle x:"); Serial.print(Angle1); Serial.print("\n");
+  //Serial.print("Angle y: "); Serial.print(Angle2); Serial.print("\n-------------\n");
+
+  //Serial.print(Angle1);Serial.print(",");Serial.print(Angle2);Serial.print("\n");
+  
+
+  //delay(10);//Nuestra dt sera pues 0.010 que es el intervalo de tiempo en cada medida
+  //Fin Loop MPU
+
+  //Inicio Loop Joystick
+  xPosicion = analogRead(xPin);
+
+  yPosicion = analogRead(yPin);
+
+  EstadoPulsador = digitalRead(Pulsador);
+
+  Serial.println(String(Angle1)+","+String(Angle2)+","+String(Angle3)+","+String(xPosicion)+","+String(yPosicion)+","+(EstadoPulsador));
+}
---
+++ Assets/Scene/EscenePruebas/PruebaMPU/MoviCursor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+
+public class MoviCursor : MonoBehaviour {
+	public static Thread oThread;
+	public static SerialPort serial;
+	public static string PUERTO = "COM4";
+
+	public float angulox = 0.0f;
+	public float anguloy = 0.0f;
+	public float anguloz = 0.0f;
+	public int ejeX = 0;
+	public int ejeY = 0;
+	public int pulsador = 1;
+
+	public float velocidad = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+		oThread = new Thread(new ParameterizedThreadStart(escucharPuertoSerialArduino));
+		// Start the thread
+		oThread.Start(this);
+		while(!oThread.IsAlive); // wait for thread to start
+		Debug.Log("Thread started : " + Time.realtimeSinceStartup);
+	}
+
+	void EndThread() {
+		try {
+			if(oThread.IsAlive) {
+				oThread.Abort();
+				Debug.Log("Trying to abort");
+			}
+		} catch {
+			Debug.Log("Aborting thread failed");
+		}
+	}
+
+	/**
+	 * Loop principal de escucha de puerto arduino 
+	 */
+	public void escucharPuertoSerialArduino(object parent) {
+		string ultimoData = "";
+
+		Debug.Log ("Escuchando puerto "+PUERTO+"...");
+		serial = new SerialPort(PUERTO,9600);
+		serial.ReadTimeout = 10000;
+		while(true) {
+			//Debug.Log ("Puerto Abierto "+serial.IsOpen);
+			if (!serial.IsOpen) {
+				Debug.Log ("Abriendo Puerto...");
+				serial.Open ();
+			}
+			//Debug.Log("["+serial.ReadLine()+"]");
+
+
+			String dataRaw = serial.ReadLine();
+			ultimoData = dataRaw;
+			try {
+				string[] datoArray = dataRaw.Split(",".ToCharArray(),6);
+				((MoviCursor) parent).angulox = float.Parse(datoArray[0]);
+				((MoviCursor) parent).anguloy = float.Parse(datoArray[1]);
+				((MoviCursor) parent).anguloz = float.Parse(datoArray[2]);
+
+
+				((MoviCursor) parent).ejeX = int.Parse(datoArray[3]);
+				((MoviCursor) parent).ejeY = int.Parse(datoArray[4]);
+				((MoviCursor) parent).pulsador = int.Parse(datoArray[5]);
+			} catch (Exception e) {
+				//Debug.Log ("Error:"+e.Message+ "["+ultimoData+"]");
+			}
+		}
+
+	}
+
+	void OnDestroy() {
+		EndThread();
+	}
+
+	void OnApplicationQuit(){
+		Debug.Log ("Terminando aplicacion");
+		serial.Close ();
+		EndThread();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//this.transform.localEulerAngles = new Vector2 (0,0);
+		//Quaternion target = Quaternion.Euler(angulox,anguloy,0);
+		//transform.rotation = target;
+
+		if (angulox < 0) {
+			this.transform.Translate (Vector2.left * Time.deltaTime, Space.World);
+		} else {
+			this.transform.Translate (Vector2.right * Time.deltaTime, Space.World);
+		}
+		if (anguloy < 0) {
+			this.transform.Translate (Vector2.down * Time.deltaTime, Space.World);
+		} else {
+			this.transform.Translate (Vector2.up * Time.deltaTime, Space.World);
+		}
+
+
+	//	if (ejeY > 540 && ejeY != 0) {
+	//		this.transform.position = this.transform.position - Camera.main.transform.forward * velocidad;
+	//	} else if (ejeY < 440 && ejeY != 0) {
+	//		this.transform.position = this.transform.position + Camera.main.transform.forward * velocidad;	
+	//	} else {
+			//Esta centrado en eje Y
+	//	}
+
+	//	if (ejeX > 540 && ejeY != 0) {
+	//		this.transform.position = this.transform.position + Camera.main.transform.right * velocidad;
+	//	} else if (ejeX < 440 && ejeY != 0) {
+	//		this.transform.position = this.transform.position - Camera.main.transform.right * velocidad;
+	//	} else {
+			//Esta centrado en eje Y
+	//	}
+	}
+}
